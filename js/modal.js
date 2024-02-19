9d1c1f9d80b2a5e8255d2235fb280e529640a1a9
yakyar <155535426+yakyar@users.noreply.github.com>
2024-02-19 15:36:17 +0200

    мм

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+// const modal = document.querySelector(".backdrop");
+// const modalBtnOpen = document.querySelector(".modal-btn-open");
+// const modalBtnOpenn = document.querySelector(".modal-btn-openn");
+// const modalBtnClose = document.querySelector(".modal-btn-close");
+
+// const toggleModal = () => modal.classList.toggle("is-hidden");
+
+// modalBtnOpen.addEventListener("click", toggleModal);
+// modalBtnOpenn.addEventListener("click", toggleModal);
+// modalBtnClose.addEventListener("click", toggleModal);
+const modal = document.querySelector(".backdrop");
+
+const modalBtnOpen = document.querySelectorAll(".modal-btn-open");
+const modalBtnClose = document.querySelectorAll(".modal-btn-close");
+
+const toggleModal = () => modal.classList.toggle("is-hidden");
+
+modalBtnOpen.forEach(function (e) {
+  e.addEventListener("click", toggleModal);
+});
+
+modalBtnClose.forEach(function (e) {
+  e.addEventListener("click", toggleModal);
+});
